295bcb2e076e27284db206348136be6f302faff6
stephenAmp <stephen013amponsem@gmail.com>
2026-09-24 21:02:29 +0000

    enabled CORS for frontend

--- app/main.py
+++ app/main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI;
 from app.api.routes.urls import router as url_router
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI(title = "URL Shortener", version="1.0.0")
 
+origins = [
+    "http://localhost:5173",  
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get("/health")
 def get_health():
     return {"status":"ok"}
